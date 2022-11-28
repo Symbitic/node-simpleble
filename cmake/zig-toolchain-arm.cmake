019fbd29585752c87a798aeb10ce0fe9a11bb682
Alex Shaw <alex.shaw.as@gmail.com>
2022-11-27 18:02:53 -0800

    ci: add zig cross compile

---
+++ cmake/zig-toolchain-arm.cmake
@@ -0,0 +1,41 @@
+if(CMAKE_GENERATOR MATCHES "Visual Studio")
+  message(FATAL_ERROR "Visual Studio generator not supported, use: cmake -G Ninja")
+endif()
+set(CMAKE_SYSTEM_NAME "Linux")
+set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+set(CMAKE_C_COMPILER "zig" cc -target arm-linux-musl)
+set(CMAKE_CXX_COMPILER "zig" c++ -target arm-linux-musl)
+
+if(WIN32)
+  set(SCRIPT_SUFFIX ".cmd")
+else()
+  set(SCRIPT_SUFFIX ".sh")
+endif()
+set(CMAKE_AR "${CMAKE_CURRENT_LIST_DIR}/zig-ar${SCRIPT_SUFFIX}")
+set(CMAKE_RANLIB "${CMAKE_CURRENT_LIST_DIR}/zig-ranlib${SCRIPT_SUFFIX}")
+
+set(CMAKE_FIND_ROOT_PATH "${CMAKE_CURRENT_SOURCE_DIR}/temp/arm64")
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+
+# DBus
+set(DBus1_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/temp/arm64" CACHE STRING "Root")
+set(DBus1_LIBRARY "${CMAKE_CURRENT_SOURCE_DIR}/temp/arm64/lib/libdbus-1.a"
+  CACHE STRING "DBus library")
+set(DBus1_LIBRARIES
+  "${CMAKE_CURRENT_SOURCE_DIR}/temp/arm64/lib/libdbus-1.a"
+  "${CMAKE_CURRENT_SOURCE_DIR}/temp/arm64/lib/liblzma.a"
+  "${CMAKE_CURRENT_SOURCE_DIR}/temp/arm64/lib/liblz4.a"
+  "${CMAKE_CURRENT_SOURCE_DIR}/temp/arm64/lib/libgpg-error.a"
+  "${CMAKE_CURRENT_SOURCE_DIR}/temp/arm64/lib/libgcrypt.a"
+  CACHE STRING "DBus libraries")
+set(DBus1_INCLUDE_DIRS "${CMAKE_CURRENT_SOURCE_DIR}/temp/arm64/include/dbus-1.0"
+  CACHE STRING "DBus include dirs")
+set(DBus1_FOUND TRUE CACHE BOOL "DBus found")
+
+set(CMAKE_C_FLAGS "-pthread -Wl,-Bsymbolic -I${DBus1_INCLUDE_DIRS}" CACHE STRING "Compiler Flags")
+set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "Compiler Flags")
+set(CMAKE_SHARED_LINKER_FLAGS "-Wl,-Bsymbolic" CACHE STRING "Link flags")
---
+++ extract.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Exit on failure
+set -e
+
+if [ $# -ne 1 ]
+then
+  echo "No arch given"
+  exit 1
+fi
+
+arch=$1
+
+# Validate arch.
+case $arch in
+   arm64|armhf)
+     ;;
+   *)
+     echo "$arch is not a recognized architecture"
+     exit 1
+     ;;
+esac
+
+echo "Extracting dbus for arch"
+
+mkdir -p temp
+cd temp
+rm -rf root "$arch"
+rm -rf *.deb
+
+LIBDBUS_DEB="libdbus-1-dev_1.12.16-2ubuntu2.3_${arch}.deb"
+LIBLZMA_DEB="liblzma-dev_5.2.4-1ubuntu1.1_${arch}.deb"
+LIBLZ4_DEB="liblz4-dev_1.9.2-2ubuntu0.20.04.1_${arch}.deb"
+LIBGPG_ERROR_DEB="libgpg-error-dev_1.37-1_${arch}.deb"
+LIBGCRYPT_DEB="libgcrypt20-dev_1.8.5-5ubuntu1.1_${arch}.deb"
+LIBSYSTEMD_DEB="libsystemd-dev_245.4-4ubuntu3.15_${arch}.deb"
+
+LIBDBUS_URL="http://ports.ubuntu.com/pool/main/d/dbus/${LIBDBUS_DEB}"
+LIBLZMA_URL="http://ports.ubuntu.com/pool/main/x/xz-utils/${LIBLZMA_DEB}"
+LIBLZ4_URL="http://ports.ubuntu.com/pool/main/l/lz4/${LIBLZ4_DEB}"
+LIBGPG_ERROR_URL="http://ports.ubuntu.com/pool/main/libg/libgpg-error/${LIBGPG_ERROR_DEB}"
+LIBGCRYPT_URL="http://ports.ubuntu.com/pool/main/libg/libgcrypt20/${LIBGCRYPT_DEB}"
+LIBSYSTEMD_URL="http://ports.ubuntu.com/pool/main/s/systemd/${LIBSYSTEMD_DEB}"
+
+echo "Downloading..."
+wget -q -c "${LIBDBUS_URL}"
+wget -q -c "${LIBLZMA_URL}"
+wget -q -c "${LIBLZ4_URL}"
+wget -q -c "${LIBGPG_ERROR_URL}"
+wget -q -c "${LIBGCRYPT_URL}"
+wget -q -c "${LIBSYSTEMD_URL}"
+
+echo "Extracting..."
+dpkg-deb -x "${LIBDBUS_DEB}" root
+dpkg-deb -x "${LIBLZMA_DEB}" root
+dpkg-deb -x "${LIBLZ4_DEB}" root
+dpkg-deb -x "${LIBGPG_ERROR_DEB}" root
+dpkg-deb -x "${LIBGCRYPT_DEB}" root
+dpkg-deb -x "${LIBSYSTEMD_DEB}" root
+
+mkdir -p ${arch}/lib
+
+for f in `find ./root -iname "*.a"`
+do
+  mv "$f" "$arch/lib/"
+done
+
+mv root/usr/include ${arch}/include
+if [ "$arch" == "arm64" ]; then mv "root/usr/lib/aarch64-linux-gnu/dbus-1.0/include/dbus/dbus-arch-deps.h" "${arch}/include/dbus-1.0/dbus/"; fi
+if [ "$arch" == "armhf" ]; then mv "root/usr/lib/arm-linux-gnueabihf/dbus-1.0/include/dbus/dbus-arch-deps.h" "${arch}/include/dbus-1.0/dbus/"; fi
+
+rm -rf root
+rm -rf *.deb
+
+cd ..
